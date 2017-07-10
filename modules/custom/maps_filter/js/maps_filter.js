d6161cccda6695a84eb9055214a61d343e7a89d9
Ivan_Trokhanenko <gamernext@gmail.com>
2017-07-10 09:37:29 +0300

    Update map

---
+++ modules/custom/maps_filter/js/maps_filter.js
@@ -0,0 +1,57 @@
+(function ($) {
+
+  /**
+   * Set active class on Views AJAX filter
+   * on selected category
+   */
+  Drupal.behaviors.exposedfilter_buttons = {
+    attach: function(context, settings) {
+      $('.filter-tab a').on('click', function(e) {
+        e.preventDefault();
+
+        // Get ID of clicked item
+        var id = $(e.target).attr('id');
+
+        // Set the new value in the SELECT element
+        var filter = $('#views-exposed-form-karta-block-1 select[name="field_regions_target_id"]');
+        filter.val(id);
+
+        // Unset and then set the active class
+        $('.filter-tab a').removeClass('active');
+        $(e.target).addClass('active');
+
+        // Do it! Trigger the select box
+        //filter.trigger('change');
+        $('#views-exposed-form-karta-block-1 select[name="field_regions_target_id"]').trigger('change');
+        $('#views-exposed-form-karta-block-1 button.form-submit').trigger('click');
+
+      });
+    }
+  };
+
+
+  jQuery(document).ajaxComplete(function(event, xhr, settings) {
+
+    var filter_id = $('#views-exposed-form-karta-block-1 select[name="field_regions_target_id"]').find(":selected").val();
+    $('.filter-tab a').removeClass('active');
+    $('.filter-tab').find('#' + filter_id).addClass('active');
+
+  });
+
+  Drupal.behaviors.read_more_buttons = {
+    attach: function(context, settings) {
+      var read_more = "<span class='read-more'>Еще...</span>";
+
+      //var leng = $('.pop-list li').length;
+
+      $('.pop-list').once().after(read_more);
+
+      $('.read-more').once().click(function(){
+        $('.pop-list').css("height", "100px");
+        $('.read-more').remove();
+      });
+    }
+  };
+
+
+})(jQuery);
